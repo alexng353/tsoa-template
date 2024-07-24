db07da7b1d3e8ac808b34b0afc21b45b34b5807b
Alexander Ng <alex@ngland.net>
2024-07-25 00:41:20 +0700

    add standard tsoa error handling

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,41 @@
-// src/app.ts
-import express, { json, urlencoded } from "express";
+import express, {
+  Response as ExpressResponse,
+  Request as ExpressRequest,
+  NextFunction,
+} from "express";
 import { RegisterRoutes } from "../build/routes";
+import { ValidateError } from "tsoa";
 
 export const app = express();
 
-// Use body parser to read sent json payloads
 app.use(
-  urlencoded({
+  express.urlencoded({
     extended: true,
   }),
 );
-app.use(json());
+app.use(express.json());
 
 RegisterRoutes(app);
+
+app.use(function errorHandler(
+  err: unknown,
+  req: ExpressRequest,
+  res: ExpressResponse,
+  next: NextFunction,
+): ExpressResponse | void {
+  if (err instanceof ValidateError) {
+    console.warn(`Caught Validation Error for ${req.path}:`, err.fields);
+    return res.status(422).json({
+      message: "Validation Failed",
+      details: err?.fields,
+    });
+  }
+
+  if (err instanceof Error) {
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+
+  next();
+});
